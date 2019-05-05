69d4b5a21ad3fe7ce4bc3daa7a7c8ab35956d5fc
Matthias Bauer <moeffju@moeffju.net>
2019-05-05 18:24:45 +0200

    Add dry run and test mode

--- sync.rb
+++ sync.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 require 'rubygems'
 require 'bundler/setup'
@@ -19,7 +20,14 @@ Trello.configure do |setting|
   setting.member_token = config[:trello][:member_token]
 end
 
-BOARD_ID = '5ccc5ac4a9ae524b9d9ad792'
+MODE = ARGV.any? { |arg| arg =~ /--prod/ } ? 'prod' : 'test'
+DRY_RUN = ARGV.any? { |arg| arg =~ /--dry-run|-n/ }
+
+if MODE == 'prod'
+  BOARD_ID = '5ccc5ac4a9ae524b9d9ad792'
+else
+  BOARD_ID = '5ccf0c16c3a66923be391671'
+end
 
 LINK_BASE = 'https://19.re-publica.com'
 SESSIONS_URL = 'https://www.elfengleich.de/rp19/sessions19.json'
@@ -43,6 +51,8 @@ RUN = Time.now.iso8601[11..18]
 UTC = Time.find_zone('UTC')
 
 log "Run started: #{RUN}"
+log "TEST MODE, using test board; pass `--prod` if you mean it" if MODE != 'prod'
+log "DRY RUN, not creating or updating cards" if DRY_RUN
 
 def convert_to_text(html, line_length = 65, from_charset = 'UTF-8')
   txt = html
@@ -106,7 +116,7 @@ log "Preparing Trello board"
 board = Trello::Board.find(BOARD_ID)
 
 lists = board.lists
-log "Lists found: #{lists.map(&:name).join(', ')}"
+log "Lists found: [#{lists.map(&:name).join(', ')}]"
 backlog = lists.select{ |l| l.name == 'Backlog' }.first || Trello::List.create(board_id: board.id, name: 'Backlog')
 day_lists = {}
 conference_dates.each_with_index do |date, i|
@@ -119,16 +129,20 @@ log "Created backlog and day lists"
 labels = board.labels
 log "Setting up labels"
 language_labels = {}
-LANGUAGE_COLOR_MAP.keys.each do |lang|
-  language_labels[lang] = labels.select{ |l| l.name == lang }.first || Trello::Label.create(board_id: board.id, name: lang, color: LANGUAGE_COLOR_MAP[lang])
+unless DRY_RUN
+  LANGUAGE_COLOR_MAP.keys.each do |lang|
+    language_labels[lang] = labels.select{ |l| l.name == lang }.first || Trello::Label.create(board_id: board.id, name: lang, color: LANGUAGE_COLOR_MAP[lang])
+  end
+  log "Language labels: [#{language_labels.map{ |k,v| "#{v.name} (#{v.color})" }.join(', ')}]"
 end
-log "Language labels: #{language_labels.map{ |k,v| "#{v.name} (#{v.color})" }.join(', ')}"
 format_labels = {}
-FORMAT_COLOR_MAP.keys.each do |fmt|
-  format_labels[fmt] = labels.select{ |l| l.name == fmt }.first || Trello::Label.create(board_id: board.id, name: fmt, color: FORMAT_COLOR_MAP[fmt])
-  format_labels[fmt].color = FORMAT_COLOR_MAP[fmt]
+unless DRY_RUN
+  FORMAT_COLOR_MAP.keys.each do |fmt|
+    format_labels[fmt] = labels.select{ |l| l.name == fmt }.first || Trello::Label.create(board_id: board.id, name: fmt, color: FORMAT_COLOR_MAP[fmt])
+    format_labels[fmt].color = FORMAT_COLOR_MAP[fmt]
+  end
+  log "Format labels: [#{format_labels.map{ |k,v| "#{v.name} (#{v.color})" }.join(', ')}]"
 end
-log "Format labels: #{format_labels.map{ |k,v| "#{v.name} (#{v.color})" }.join(', ')}"
 
 log "Getting cards"
 cards = board.cards(filter: :open)
@@ -189,16 +203,24 @@ translated_sessions.each do |session|
     card = cards_map[session['nid']]
     # update_fields did not work for some reason, but this does
     data.each { |k,v| card.send(k.to_s+'=', v) }
-    card.update!
-    card.move_to_list session_list
+    card.update! unless DRY_RUN
+    card.move_to_list session_list unless DRY_RUN
     log "Card (updated): #{card.name}"
   else
-    card = Trello::Card.create(list_id: backlog.id, **data, list_id: session_list.id)
-    log "Card (new): #{card.name}"
+    unless DRY_RUN
+      card = Trello::Card.create(list_id: backlog.id, **data, list_id: session_list.id)
+      log "Card (new): #{card.name}"
+    else
+      log "DRY RUN: Not saving Card (new): #{data[:name]}"
+    end
   end
-  card.add_label format_labels[session['format']] rescue nil
-  card.add_label language_labels[session['language']] rescue nil
-  card.save
+  unless DRY_RUN
+    card.add_label format_labels[session['format']] rescue nil
+    card.add_label language_labels[session['language']] rescue nil
+    card.save
+  else
+    log "DRY RUN: Not saving card changes"
+  end
 end
 
 log "Done!"
